05eb02b3ebb5484fef17f0bbb0f54c2166d9529e
s7s <s7s@gmail.com>
2025-02-07 18:24:13 +0200

    welcome

--- index.js
+++ index.js
@@ -7,4 +7,5 @@ import { bootstrap } from "./src/app.controller.js";
 const app = express();
 const port = process.env.PORT;
 bootstrap(app,express)
+// ******************
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
